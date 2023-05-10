363361c3eb2e19b37f7446ded7190f3c646fc84a
sarahkschr <112958545+sarahkschr@users.noreply.github.com>
2023-05-10 11:34:38 +0200

    Add files via upload
    Random search parameters

---
+++ logistic regression/lr_random_search.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, train_test_split
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.preprocessing import RobustScaler
+
+# Load the dataset
+filename = "heart_failure_clinical_records_dataset.csv"
+heart_df = pd.read_csv(filename)
+
+# Split the dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    heart_df.drop('DEATH_EVENT', axis=1), heart_df['DEATH_EVENT'],
+    test_size=0.2, random_state=3)
+
+# scale
+scaler = RobustScaler()
+X_train = scaler.fit_transform(X_train)
+
+# Define the hyperparameter space to search over
+param_dist = {'penalty': ['l1', 'l2', 'elasticnet', 'None'],
+              'dual': [True, False],
+              'C': np.logspace(-3, 3, 7),
+              'fit_intercept': [True, False],
+              'intercept_scaling': np.logspace(-3, 3, 7),
+              'class_weight': ['balanced', None],
+              'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],
+              'max_iter': [100, 500, 1000],
+              'multi_class': ['auto', 'ovr', 'multinomial']}
+
+
+# Create a logistic regression model
+lr = LogisticRegression()
+
+# Create a random search object
+random_search = RandomizedSearchCV(lr, param_distributions=param_dist, cv=5, random_state=3)
+
+# Fit the model to the training data
+random_search.fit(X_train, y_train)
+
+# Print the best hyperparameters and score
+print("Best hyperparameters: ", random_search.best_params_)
+print("Best score: ", random_search.best_score_)
